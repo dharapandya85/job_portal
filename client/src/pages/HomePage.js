70eb250ea742f4e1f0443655d8cf5dac1e8ea539
dharapandya85 <dharapandya@gmail.com>
2025-04-19 14:41:04 +0530

    added dashboard page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,13 +1,31 @@
 import React from 'react';
-
+import {Link} from "react-router-dom";
+import '../styles/Homepage.css';
 const HomePage = () => {
   return (
-    <div>
-        <h1 className="text-primary">
-            HomePage
-        </h1>
+    <>
+    <video autoPlay muted loop id="myVideo">
+      <source src="/assets/videos/video.mp4" type="video/mp4" />
+    </video>
+        <div className="content">
+          <div className="card w-25">
+            <img src="/assets/images/logo/logo.png" alt="logo"/>
+         <hr/>
+          <div className="card-body" style={{marginTop:"-60px"}}>
+            <h5 className="card-title">India's No #1 Career Platform </h5>
+            <p className="card-text">
+              Search and manage your jobs with ease.Free and Open Source Job Portal Application.
+            </p>
+            <div className="d-flex justify-content-between mt-5">
+              <p>Not a user Register <Link to="/register">Here !</Link> </p>
+              <p> <Link to="/login" className="myBtn">Login</Link> </p>
+            </div>
+            </div>
+          </div>
+        </div>
+        
       
-    </div>
+    </>
   )
 }
 
